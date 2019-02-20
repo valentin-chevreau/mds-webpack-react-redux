eeb6ae87727e695dd18243aa882715dabbf9a884
Valentin CHEVREAU <vchevreau37@gmail.com>
2019-02-20 21:44:27 +0100

    login displau in live informations entered

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class Login extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      username: '',
-      password: ''
+      username: props.user.data.name,
+      password: props.user.data.password
     }
+    console.log('initial-state', this.state)
 
     this.handleUsernameChange = this.handleUsernameChange.bind(this)
     this.handlePasswordChange = this.handlePasswordChange.bind(this)
@@ -28,39 +30,72 @@ class Login extends Component {
     const { update } = this.props
     const { username } = this.state
     const { password } = this.state
-    update(username)
-    update(password)
+    const { usernameUpdate } = update(username)
+    const { passwordUpdate } = update(password)
+    return (usernameUpdate, passwordUpdate)
+  }
+
+  dataToString() {
+    // Props : state globale de l'app
+    // this.state : state du component
+    const { user } = this.props
+    const { data } = user
+    console.log('D', data)
+    let connectedUser = ''
+    data.map((request) => {
+      if (data.name === this.handleSubmit) {
+        connectedUser = request.id + request.name
+        return ''
+      }
+      return ''
+    })
+    console.log('connectedUser', connectedUser)
   }
 
   render() {
     const { username } = this.state
     const { password } = this.state
 
+    console.log('userInRender', username)
+    console.log('passwordInRender', password)
+
     return (
       <div className="container">
-        <form onSubmit={this.handleSubmit}>
-          <div className="row">
-            <fieldset>
-              <legend>Connection to user account</legend>
-              <div className="col">
-                Username
-                <input type="text" className="form-control" name="username" value={username} onChange={this.handleUsernameChange} />
-              </div>
-              <div className="col">
-                Password
-                <input type="password" className="form-control" name="password" autoComplete="" value={password} onChange={this.handlePasswordChange} />
+        <section className="col-lg-4">
+          <form onSubmit={this.handleSubmit}>
+            <div className="row">
+              <fieldset>
+                <legend>
+                  Connexion à un compte utilisateur
+                </legend>
+                <div className="col">
+                  Username
+                  <input type="text" className="form-control" name="username" value={username} onChange={this.handleUsernameChange} />
+                </div>
+                <div className="col">
+                  Password
+                  <input type="password" className="form-control" name="password" autoComplete="" value={password} onChange={this.handlePasswordChange} />
+                  <br />
+                </div>
+                <input type="submit" value="Submit" className="btn btn-primary mb-2" />
                 <br />
-              </div>
-              <input type="submit" value="Submit" className="btn btn-primary mb-2" />
-              <br />
-              {` ${username} `}
-              {` ${password} `}
-            </fieldset>
-          </div>
-        </form>
+              </fieldset>
+            </div>
+          </form>
+        </section>
+        <aside className="col-lg-4">
+          <p>
+            This is the actual username:
+            {` ${username} `}
+          </p>
+          <p>
+            This is the actual passwd:
+            {` ${password} `}
+          </p>
+        </aside>
       </div>
     )
   }
 }
 
-export default Login
+export default connect(state => state)(Login)
