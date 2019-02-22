c405c433f7bcfaed83ef6fd1b1e0eb23519e1a31
Valentin CHEVREAU <vchevreau37@gmail.com>
2019-02-22 13:20:38 +0100

    login update errors

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
 
 class Login extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      username: props.user.data.name,
-      password: props.user.data.password
+      username: '',
+      password: ''
     }
-    console.log('initial-state', this.state)
 
     this.handleUsernameChange = this.handleUsernameChange.bind(this)
     this.handlePasswordChange = this.handlePasswordChange.bind(this)
@@ -30,59 +28,39 @@ class Login extends Component {
     const { update } = this.props
     const { username } = this.state
     const { password } = this.state
-    const { usernameUpdate } = update(username)
-    const { passwordUpdate } = update(password)
-    return (usernameUpdate, passwordUpdate)
-  }
-
-  dataToString() {
-    // Props : state globale de l'app
-    // this.state : state du component
-    const { user } = this.props
-    const { data } = user
-    console.log('D', data)
-    let connectedUser = ''
-    data.map((request) => {
-      if (data.name === this.handleSubmit) {
-        connectedUser = request.id + request.name
-        return ''
-      }
-      return ''
-    })
-    console.log('connectedUser', connectedUser)
+    update(username)
+    update(password)
   }
 
   render() {
     const { username } = this.state
     const { password } = this.state
 
-    console.log('userInRender', username)
-    console.log('passwordInRender', password)
-
     return (
       <div className="container">
-        <div className="row">
-          <form className="form-row" onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit}>
+          <div className="row">
             <fieldset>
               <legend>Connection to user account</legend>
-              <div className="form-group col-lg-8">
+              <div className="col">
                 Username
                 <input type="text" className="form-control" name="username" value={username} onChange={this.handleUsernameChange} />
               </div>
-              <div className="form-group col-lg-8">
+              <div className="col">
                 Password
-                <input type="password" className="form-control" name="password" autoComplete="" value={password} onChange={this.handlePasswordChange} />
+                <input type="password" className="form-control" name="password" value={password} onChange={this.handlePasswordChange} />
                 <br />
               </div>
-              <div className="form-group col-lg-8">
-                <input type="submit" value="Submit" className="btn btn-primary mb-2" />
-              </div>
+              <input type="submit" value="Submit" className="btn btn-primary mb-2" />
+              <br />
+              {` ${username} `}
+              {` ${password} `}
             </fieldset>
-          </form>
-        </div>
+          </div>
+        </form>
       </div>
     )
   }
 }
 
-export default connect(state => state)(Login)
+export default Login
