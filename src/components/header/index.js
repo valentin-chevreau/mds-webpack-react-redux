023de53386a894aa0a8d00a62881574e3d15d73d
Valentin CHEVREAU <vchevreau37@gmail.com>
2019-03-18 15:22:03 +0100

    specific links when signin/signout

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,14 +4,33 @@ import { Link } from 'react-router-dom'
 class Header extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <Link className="navbar-brand" to="/">Projet React-Redux</Link>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <Link className="nav-link" to="/reader">Lire</Link>
-            <Link className="nav-link" to="/qrcode">QrCode</Link>
-            <Link className="nav-link" to="/login">Sign-up</Link>
-          </ul>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="#">School absences</Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            {
+              localStorage.getItem('user') === null ? (
+                <Link className="nav-item nav-link" to="/login">Sign in</Link>
+              ) : (
+                <div style={{ display: 'flex' }}>
+                  <Link className="nav-item nav-link" to="/reader">Lire</Link>
+                  <Link className="nav-item nav-link" to="/qrcode">QrCode</Link>
+                  <Link className="nav-item nav-link" to="/logout">Logout</Link>
+                </div>
+              )
+            }
+          </div>
         </div>
       </nav>
     )
